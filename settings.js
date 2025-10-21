0047ec0af139757c27dd46279956c54f1f33c078
k2g52gckvp-a11y <k2g52gckvp@privaterelay.appleid.com>
2025-10-21 12:41:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
+  packname: 'LAVISH Bot',
+  author: 'LAVISH BOT‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'LAVISH', // Your name
+  ownerNumber: '2348134053093', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
